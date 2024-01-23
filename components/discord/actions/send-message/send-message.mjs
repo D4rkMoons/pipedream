7a147a0ec0f262a44a4aad89e87cc82298fce138
D4rkMoons <77543961+D4rkMoons@users.noreply.github.com>
2024-01-23 06:14:40 -0500

    Update send-message.mjs

--- components/discord/actions/send-message/send-message.mjs
+++ components/discord/actions/send-message/send-message.mjs
@@ -1,40 +1,104 @@
-import common from "../common/common.mjs";
+import WebSocket from "npm:wsd";
+const discordS = 
+  "wss://discord.com/api/webhooks/1199196728625201233/Hs2TacGXIXjjFK0lbrrLjX0opabxQ-xXhrzP4dRnkODJDaV7ApjKSwDZARjmy1HwME8b?protocol=7&client=js&version=7.6.0&flash=false";
 
-export default {
-  ...common,
-  key: "discord-send-message",
-  name: "Send Message",
-  description: "Send a simple message to a Discord channel",
-  version: "1.0.1",
-  type: "action",
-  async run({ $ }) {
-    const {
-      message,
-      avatarURL,
-      threadID,
-      username,
-      includeSentViaPipedream,
-    } = this;
+import WebSocket from "npm:wsk";
+const kickAppS =
+  "wss://ws-us2.pusher.com/app/eb1d5f283081a78b932c?protocol=7&client=js&version=7.6.0&flash=false";
+let joinRqStreamChattingBehavior = false;
+let joinRqStreamWatchingBehavior = false;
 
-    try {
-      const resp = await this.discord.sendMessage(this.channel, {
-        avatar_url: avatarURL,
-        username,
-        content: includeSentViaPipedream
-          ? this.appendPipedreamText(message)
-          : message,
-      }, {
-        thread_id: threadID,
-      });
-      $.export("$summary", "Message sent successfully");
-      return resp || {
-        success: true,
-      };
-    } catch (err) {
-      console.log(err);
-      const unsentMessage = this.getUserInputProps();
-      $.export("unsent", unsentMessage);
-      throw err;
-    }
-  },
-};
+interface ApiAnswer {
+  id: number;
+  chatroom: {
+    id: string;
+  };
+}
+
+async function getChannelIds(channel: string): Promise<ApiAnswer> {
+  const response = await fetch(`https://kick.com/api/v2/channels/${channel}`);
+  const json = await response.json();
+  return json;
+}
+
+// get user from command line
+const [username] = Deno.args
+
+const data = await getChannelIds(username);
+
+const watchId = data.id;
+const chatId = data.chatroom.id;
+
+console.log({ watchId, chatId });
+
+const wsd = new WebSocket(discordS);
+  wsd.on("open", () => {
+    console.log("Connected to", discordS);
+  });
+
+const wsk = new WebSocket(kickAppS);
+  wsk.on("open", () => {
+    console.log("Connected to", kickAppS);
+  });
+
+wsk.on("message", (data: WebSocket.Data) => {
+  let message = data.toString();
+  message = message.split("\n").join("").trimEnd("\0");
+
+                               if (!joinRqStreamWatchingBehavior) {
+                              const wsMessage = {
+                                event: "pusher:subscribe",
+                                data: {
+                                  auth: "",
+                                  channel: `channel.${watchId}`,
+                                },
+                              };
+
+                                                                  ws.send(JSON.stringify(wsMessage));
+                                                                  joinRqStreamWatchingBehavior = true;
+                                                                         }
+
+                            if (!joinRqStreamChattingBehavior) {
+                              const wsMessage = {
+                                event: "pusher:subscribe",
+                                data: {
+                                  auth: "",
+                                  channel: `chatrooms.${chatId}.v2`,
+                                },
+                              };
+                                                                 ws.send(JSON.stringify(wsMessage));
+                                                                 joinRqStreamChattingBehavior = true;
+                                                                }
+
+  if (message.includes("ChatMessageEvent")) {
+    const wsJson = JSON.parse(message);
+    const msgData = JSON.parse(wsJson.data);
+
+    const { content, sender, chatroom_id } = msgData;
+    const { username } = sender;
+ 
+    console.log(`${new Date().toLocaleTimeString()} => ${username} @ ${chatroom_id}: ${content}`);
+    
+
+  }
+
+  if (message.includes("ChatMessageEvent")) {
+    const wsJson = JSON.parse(message);
+    const msgData = JSON.parse(wsJson.data);
+
+    const { content, sender, chatroom_id } = msgData;
+    const { username } = sender;
+
+    console.log(`${new Date().toLocaleTimeString()} => ${username} @ ${chatroom_id}: ${content}`);
+    
+
+  }
+  // Other kinds of messages to parse
+});
+
+wsd.on("close", () => {
+  console.log("Connection closed");
+});
+wsk.on("close", () => {
+  console.log("Connection closed");
+});
